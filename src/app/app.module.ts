3852e94bdcc5c8ac5e4b518a86aef46c596746b1
rbalducci1986 <rbalducci1986@gmail.com>
2021-03-05 15:11:05 -0600

    commit to master test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
+import { SharedService } from './shared.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './category/category.component';
+import { ShowCatComponent } from './category/show-cat/show-cat.component';
+import { AddEditCatComponent } from './category/add-edit-cat/add-edit-cat.component';
+import { ItemComponent } from './item/item.component';
+import { ShowItemComponent } from './item/show-item/show-item.component';
+import { AddEditItemComponent } from './item/add-edit-item/add-edit-item.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    ShowCatComponent,
+    AddEditCatComponent,
+    ItemComponent,
+    ShowItemComponent,
+    AddEditItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
